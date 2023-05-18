6155f7945bd2641fb6450d04ef86bfab61e95e70
IconicLuck <uhasib959@gmail.com>
2023-05-18 12:25:48 +0600

    added routes, design and logo for header

--- src/Shared/Header/Header.jsx
+++ src/Shared/Header/Header.jsx
@@ -3,13 +3,20 @@ import { Link } from "react-router-dom";
 
 const Header = () => {
     return (
-        <div>
-            <h1 className="text-4xl">This is Header</h1>
-            <Link to='/'>Home</Link>
-            <Link to='/allToys'>All Toys</Link>
-            <Link to='/myToys'>My Toys</Link>
-            <Link to='/addAToy'>Add A Toy</Link>
-            <Link to='/blogs'>Blogs</Link>
+        <div className="bg-amber-500">
+            <div className="flex justify-between items-center px-20 py-5">
+                <div className="flex gap-5 items-center">
+                    <img className="h-[50px] w-[50px] rounded-full" src="https://i.ibb.co/hdkf5Q7/logo.jpg" alt="" />
+                    <h2 className="text-5xl font-bold">Toy Cars</h2>
+                </div>
+                <div className="flex gap-5">
+                    <Link className="font-semibold text-xl hover:bg-gray-300 px-2 rounded-lg" to='/'>Home</Link>
+                    <Link className="font-semibold text-xl hover:bg-gray-300 px-2 rounded-lg" to='/allToys'>All Toys</Link>
+                    <Link className="font-semibold text-xl hover:bg-gray-300 px-2 rounded-lg" to='/myToys'>My Toys</Link>
+                    <Link className="font-semibold text-xl hover:bg-gray-300 px-2 rounded-lg" to='/addAToy'>Add A Toy</Link>
+                    <Link className="font-semibold text-xl hover:bg-gray-300 px-2 rounded-lg" to='/blogs'>Blogs</Link>
+                </div>
+            </div>
         </div>
     );
 };
