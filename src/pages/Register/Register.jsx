03bcbd5a99f387f7523024e512a721cdc5b7bc71
IconicLuck <uhasib959@gmail.com>
2023-05-18 12:45:40 +0600

    added submit function for login and register

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -2,6 +2,19 @@ import { Link } from "react-router-dom";
 
 
 const Register = () => {
+
+    const handleRegister = event => {
+        event.preventDefault()
+
+        const form = event.target
+        const name = form.name.value
+        const email = form.email.value
+        const password = form.password.value
+        const photo = form.photo.value
+        
+        console.log(name, email, password, photo)
+    }
+
     return (
         <div>
             <div className="w-9/12 mx-auto mt-20">
@@ -11,18 +24,30 @@ const Register = () => {
                     </div>
                     <div className="card w-full shadow-2xl">
                         <div className="card-body">
-                            <form>
+                            <form onSubmit={handleRegister}>
+                                <div className="form-control">
+                                    <label className="label">
+                                        <span className="label-text">Name</span>
+                                    </label>
+                                    <input type="text" name="name" placeholder="Name" className="input input-bordered" />
+                                </div>
                                 <div className="form-control">
                                     <label className="label">
                                         <span className="label-text">Email</span>
                                     </label>
-                                    <input type="text" name="email" placeholder="email" className="input input-bordered" />
+                                    <input type="text" name="email" placeholder="Email" className="input input-bordered" />
                                 </div>
                                 <div className="form-control">
                                     <label className="label">
                                         <span className="label-text">Password</span>
                                     </label>
-                                    <input type="text" name="password" placeholder="password" className="input input-bordered" />
+                                    <input type="text" name="password" placeholder="Password" className="input input-bordered" />
+                                </div>
+                                <div className="form-control">
+                                    <label className="label">
+                                        <span className="label-text">Photo URL</span>
+                                    </label>
+                                    <input type="text" name="photo" placeholder="Photo URL" className="input input-bordered" />
                                     <label className="label">
                                         <Link to='/login' className="label-text-alt link link-hover">Already have an account?</Link>
                                     </label>
