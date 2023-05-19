e68093aeeeafe4f693800805dc2ebaff387259e9
IconicLuck <uhasib959@gmail.com>
2023-05-19 16:50:37 +0600

    implemented delete an item from the UI

--- src/pages/MyToys/MyToys.jsx
+++ src/pages/MyToys/MyToys.jsx
@@ -1,9 +1,50 @@
+import { useContext, useEffect, useState } from "react";
+import { AuthContext } from "../../providers/AuthProvider";
+import UserSpecificToys from "./UserSpecificToys/UserSpecificToys";
 
 
 const MyToys = () => {
+    const { user } = useContext(AuthContext)
+
+    const [toys, setToys] = useState([])
+
+    useEffect(() => {
+        fetch(`http://localhost:5000/myToys/${user?.email}`)
+            .then(res => res.json())
+            .then(data => setToys(data))
+    }, [user])
+
+    console.log(user?.email)
+
     return (
         <div>
-            <h1 className="text-4xl text-center">This is My Toys component</h1>
+            <div className="overflow-x-auto w-full">
+                <table className="table w-full">
+                    {/* head */}
+                    <thead>
+                        <tr>
+                            <th>Seller Name</th>
+                            <th>Name</th>
+                            <th>Sub-category</th>
+                            <th>Price</th>
+                            <th>Available Quantity</th>
+                            <th></th>
+                            <th></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {/* row 1 */}
+                        {
+                            toys.map(toy => <UserSpecificToys
+                                key={toy._id}
+                                toy={toy}
+                                toys={toys}
+                                setToys={setToys}
+                            ></UserSpecificToys>)
+                        }
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
---
+++ src/pages/MyToys/UserSpecificToys/UserSpecificToys.jsx
@@ -0,0 +1,71 @@
+import { Link } from "react-router-dom";
+import Swal from "sweetalert2";
+
+
+const UserSpecificToys = ({ toy, toys, setToys }) => {
+
+    const { _id, name, sellerName, quantity, category, price } = toy
+
+    const handleDelete = () => {
+        Swal.fire({
+            title: 'Are you sure?',
+            text: "You won't be able to revert this!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Yes, delete it!'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                //   Swal.fire(
+                //     'Deleted!',
+                //     'Your file has been deleted.',
+                //     'success'
+                //   )
+
+                fetch(`http://localhost:5000/toyCars/${_id}`, {
+                    method: 'DELETE',
+                })
+                    .then(res => res.json())
+                    .then(data => {
+                        console.log(data)
+                        if (data.deletedCount > 0) {
+                              Swal.fire(
+                                'Deleted!',
+                                'Your file has been deleted.',
+                                'success'
+                              )
+                              const remaining = toys.filter(toy => toy._id !== _id)
+                              setToys(remaining)
+                        }
+                    })
+            }
+        })
+    }
+
+    return (
+        <tr>
+            <td>
+                <div className="flex items-center space-x-3">
+                    <div>
+                        <div className="font-bold">{sellerName}</div>
+                    </div>
+                </div>
+            </td>
+            <td>
+                {name}
+            </td>
+            <td>{category}</td>
+            <td>{price}</td>
+            <td>{quantity}</td>
+            <th>
+                <Link><button className="btn btn-ghost btn-xs">Update</button></Link>
+            </th>
+            <th>
+                <Link><button onClick={handleDelete} className="btn btn-ghost btn-xs">Delete</button></Link>
+            </th>
+        </tr>
+    );
+};
+
+export default UserSpecificToys;
